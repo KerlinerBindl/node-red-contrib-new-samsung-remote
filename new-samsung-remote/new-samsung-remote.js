7b4cd2d4b46b9a7836c7ba8b02d0a41feb512a9b
KerlinerBindl <thorstenmaass87@gmail.com>
2017-11-01 22:14:41 +0100

    Update new-samsung-remote.js

--- new-samsung-remote/new-samsung-remote.js
+++ new-samsung-remote/new-samsung-remote.js
@@ -6,6 +6,36 @@ module.exports = function (RED) {
         this.prefix = config.prefix;
         this.ip = config.ip;
         var node = this;
+        // Retrieve the config node
+        this.server = RED.nodes.getNode(config.server);
+
+        if (this.server) {
+            // Do something with:
+            //  this.server.host
+            //  this.server.pos
+            node.on('input', function (msg) {
+                msg.payload = node.prefix + node.ip + msg.payload.toLowerCase();
+                node.send(msg);
+            });
+        } else {
+            // No config node configured
+        }
+
+    }
+
+    RED.nodes.registerType("new-samsung-remote", LowerCaseNode);
+}
+
+
+/*
+module.exports = function (RED) {
+
+    function LowerCaseNode(config) {
+
+        RED.nodes.createNode(this, config);
+        this.prefix = config.prefix;
+        this.ip = config.ip;
+        var node = this;
         node.on('input', function (msg) {
             msg.payload = node.prefix + node.ip + msg.payload.toLowerCase();
             node.send(msg);
@@ -14,3 +44,4 @@ module.exports = function (RED) {
 
     RED.nodes.registerType("new-samsung-remote", LowerCaseNode);
 }
+*/
